c0c5d43ee49e932c336546ca33aab24803a69e4c
Sarah Moss <sarah-moss@live.com>
2022-05-05 16:40:35 +0000

    Add a route for /urls

--- express_server.js
+++ express_server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
+
 // tells the Express app to use EJS as its templating engine
 app.set("view engine", "ejs"); // set ejs as the view engine
 
@@ -16,12 +17,18 @@ app.get("/", (request, response) => { // '/' refers to http://localhost:8080/
   response.send('Hello!');
 });
 
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
+app.get("/urls.json", (request, response) => {
+  response.json(urlDatabase);
 });
 
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
+// When sending variables to an EJS template, we need to send them inside an object, even if we are only sending one variable. This is so we can use the key of that variable (in the above case the key is urls) to access the data within our template.
+app.get("/urls", (request, response) => {
+  const templateVars = { urls: urlDatabase };
+  response.render("urls_index", templateVars); 
+});
+
+app.get("/hello", (request, response) => {
+  response.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
 app.listen(PORT, () => {
