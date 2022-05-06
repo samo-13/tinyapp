9d6919dfeab349458f8e7df1b12e91584e227ac3
Sarah Moss <sarah-moss@live.com>
2022-05-06 15:12:45 +0000

    Clean + eslint express_server

--- express_server.js
+++ express_server.js
@@ -7,9 +7,9 @@ app.set("view engine", "ejs"); // set ejs as the view engine
 
 
 // When our browser submits a POST request, the data in the request body is sent as a Buffer. While this data type is great for transmitting data, it's not readable for us humans. To make this data readable, we will need to install another piece of middleware, body-parser.
-// The body-parser library will convert the request body from a Buffer into string that we can read. 
+// The body-parser library will convert the request body from a Buffer into string that we can read.
 // It will then add the data to the req(request) object under the key body.
-const bodyParser = require("body-parser"); 
+const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
 
 // ----------------------------------------------------------------------------------------------------
@@ -25,11 +25,11 @@ const urlDatabase = {
 // FUNCTIONS
 // ----------------------------------------------------------------------------------------------------
 
-function generateRandomString() { // random string generator
+let generateRandomString = function() { // random string generator
   const stringLength = 6;
   let string = '';
 
-   while (string.length < stringLength) {
+  while (string.length < stringLength) {
     string += Math.random().toString(36).substring(2);
     string = string.substring(0, stringLength);
   }
@@ -53,11 +53,11 @@ app.get("/urls.json", (request, response) => {
   response.json(urlDatabase[shortURL]);
 });
 
-// When sending variables to an EJS template, we need to send them inside an object, even if we are only sending one variable. 
+// When sending variables to an EJS template, we need to send them inside an object, even if we are only sending one variable.
 // This is so we can use the key of that variable (in the above case the key is urls) to access the data within our template.
 app.get("/urls", (request, response) => {
   const templateVars = { urls: urlDatabase };
-  response.render("urls_index", templateVars); 
+  response.render("urls_index", templateVars);
 });
 
 // keep above /urls/:id route definition
@@ -67,13 +67,13 @@ app.get("/urls/new", (request, response) => {
 
 app.get("/urls/:shortURL", (request, response) => { // The : in front of shortURL indicates that shortURL is a route parameter
   
-  let shortURL = request.params.shortURL // https://docs.microsoft.com/en-us/dotnet/api/system.web.httprequest.params?redirectedfrom=MSDN&view=netframework-4.8#System_Web_HttpRequest_Params
-  console.log(urlDatabase[shortURL])
+  let shortURL = request.params.shortURL; // https://docs.microsoft.com/en-us/dotnet/api/system.web.httprequest.params?redirectedfrom=MSDN&view=netframework-4.8#System_Web_HttpRequest_Params
+  console.log(urlDatabase[shortURL]);
 
-  const templateVars = { 
-    shortURL: shortURL, 
+  const templateVars = {
+    shortURL: shortURL,
     longURL: urlDatabase[shortURL]
-  }; // https://expressjs.com/en/guide/routing.html#route-parameters 
+  }; // https://expressjs.com/en/guide/routing.html#route-parameters
   response.render("urls_show", templateVars);
 });
 
@@ -89,7 +89,7 @@ app.get("/helloworld", (request, response) => {
   response.render("hello_world", templateVars);
 });
 
-// the templateVars object above contains the string 'Hello World' under the key greeting. 
+// the templateVars object above contains the string 'Hello World' under the key greeting.
 // We then pass the templateVars object to the template called hello_world.
 
 // In our hello_world.ejs file, we can display the 'Hello World!' string stored in the templateVars object by calling the key greeting:
@@ -108,16 +108,15 @@ app.listen(PORT, () => {
 
 app.post("/urls", (request, response) => {
 
-  let shortURL = generateRandomString()
-  // urlDatabase[longURL] = request.body.longURL
-  let longURL = request.body.longURL
+  let shortURL = generateRandomString();
+  let longURL = request.body.longURL;
+
   console.log(request.body.longURL); // log the POST request body to the console
   response.send(shortURL); // generates a random 6 character string
-  // console.log({shortURL: longURL})
-  urlDatabase[shortURL] = longURL
-  // return Object.assign({shortURL: longURL}, urlDatabase)
-  // console.log(urlDatabase);
+  
+  urlDatabase[shortURL] = longURL; // save the shortURL-longURL key-value pair to the urlDatabase when it receives a POST request to /urls
   console.log(urlDatabase);
+  return;
 });
 
 
@@ -135,9 +134,9 @@ app.post("/urls", (request, response) => {
 // --- To create PUT and DELETE requests we would need to use a workaround known as HTTP Method Override, but for simplicity we will make do with just GET and POST. Instead of PUT and DELETE, we will use POST.
 
 // urls_new
-// The form tag has two important attributes: action and method. 
+// The form tag has two important attributes: action and method.
 // The action attribute tells the form which URL to submit to while the method attribute tells the form which HTTP method to use when submitting the form.
-// The input tag has an important attribute as well: name. 
+// The input tag has an important attribute as well: name.
 // This attribute identifies the data we are sending; in this case, it adds the key longURL to the data we'll be sending in the body of our POST request.
 
 // The order of route definitions matters! The GET /urls/new route needs to be defined before the GET /urls/:id route. Routes defined earlier will take precedence, so if we place this route after the /urls/:id definition, any calls to /urls/new will be handled by app.get("/urls/:id", ...) because Express will think that new is a route parameter. A good rule of thumb to follow is that routes should be ordered from most specific to least specific.
