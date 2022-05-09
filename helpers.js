543296915a759713b9e9e2c4a3fa7eef6c70fa7c
Sarah Moss <sarah-moss@live.com>
2022-05-09 16:21:40 +0000

    Add mocha & chai tests for getUserByEmail function

--- helpers.js
+++ helpers.js
@@ -1,7 +1,7 @@
 // helper functions for TinyApp
 
 let getUserByEmail = function(email, users) {
-  console.log("getUserByEmail");
+  // console.log("getUserByEmail");
 
   for (let user in users) {
     if (email === users[user].email) { // was unable to get emailChecker function to work here
